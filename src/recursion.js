c8dacb7ed1fcdaadbeea58d42cba4f06d914bcda
Rashmi Baheti <baheti.rash123@gmail.com>
2017-10-06 12:28:18 -0700

    completed this.js and 2 problems from recursion
    -Rashmi Baheti

--- src/recursion.js
+++ src/recursion.js
@@ -10,12 +10,22 @@ const nFibonacci = (n) => {
 const nFactorial = (n) => {
   // factorial example: !5 = 5 * 4 * 3 * 2 * 1
   // return the factorial of `n`
+  if (n === 0) return 1;
+  return n * nFactorial(n - 1);
 };
 
 /* Extra Credit */
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  const arrOfValues = Object.values(obj);
+  const value = arrOfValues[0];
+  for (let i = 1; i < arrOfValues.length; ++i) {
+    if (value !== arrOfValues[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 /* eslint-enable no-unused-vars */
